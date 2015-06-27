7e3b4bb61c16afc96e778b465f45873c6002ca07
Pasikonik <blind.x6@gmail.com>
2015-06-27 12:52:20 +0200

    change upload do s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,18 @@
-if Rails.env.test?
-  CarrierWave.configure do |config|
-    config.storage = :file
-    config.enable_processing = false
+CarrierWave.configure do |config|
+  if Rails.env.test?
+    config.storage :file
+  else
+    config.storage :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['LIGHT_S3_ID'],
+      aws_secret_access_key: ENV['LIGHT_S3_KEY'],
+      region: 'eu-central-1'
+    }
+    if Rails.env.development?
+      config.fog_directory = ENV['LIGHT_S3_BUCKETS_DEV']
+    elsif Rails.env.production?
+      config.fog_directory = ENV['LIGHT_S3_BUCKETS_DEV']
+    end
   end
 end
