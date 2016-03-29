25cf71e3579ae46368f645b454bb16643488659d
Pasikonik <blind.x6@gmail.com>
2016-03-29 20:38:01 +0200

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150601160124) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
     t.text     "body"
@@ -24,9 +27,9 @@ ActiveRecord::Schema.define(version: 20150601160124) do
     t.datetime "updated_at"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.string   "title",            limit: 50, default: ""
@@ -39,9 +42,9 @@ ActiveRecord::Schema.define(version: 20150601160124) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id"
-  add_index "comments", ["commentable_type"], name: "index_comments_on_commentable_type"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
+  add_index "comments", ["commentable_type"], name: "index_comments_on_commentable_type", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "entries", force: :cascade do |t|
     t.string   "title"
@@ -52,7 +55,7 @@ ActiveRecord::Schema.define(version: 20150601160124) do
     t.string   "thumbnail"
   end
 
-  add_index "entries", ["user_id"], name: "index_entries_on_user_id"
+  add_index "entries", ["user_id"], name: "index_entries_on_user_id", using: :btree
 
   create_table "images", force: :cascade do |t|
     t.string   "src"
@@ -69,8 +72,8 @@ ActiveRecord::Schema.define(version: 20150601160124) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "ratings", ["user_id"], name: "index_ratings_on_user_id"
-  add_index "ratings", ["video_id"], name: "index_ratings_on_video_id"
+  add_index "ratings", ["user_id"], name: "index_ratings_on_user_id", using: :btree
+  add_index "ratings", ["video_id"], name: "index_ratings_on_video_id", using: :btree
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
@@ -82,15 +85,15 @@ ActiveRecord::Schema.define(version: 20150601160124) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: :cascade do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
   end
 
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "tutorials", force: :cascade do |t|
     t.string   "title"
@@ -120,9 +123,9 @@ ActiveRecord::Schema.define(version: 20150601160124) do
     t.string   "avatar"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "videos", force: :cascade do |t|
     t.string   "title"
@@ -133,11 +136,14 @@ ActiveRecord::Schema.define(version: 20150601160124) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "kind"
-    t.integer  "views",       default: 0
     t.float    "score",       default: 0.0
     t.string   "remote"
   end
 
-  add_index "videos", ["user_id"], name: "index_videos_on_user_id"
+  add_index "videos", ["user_id"], name: "index_videos_on_user_id", using: :btree
 
+  add_foreign_key "entries", "users"
+  add_foreign_key "ratings", "users"
+  add_foreign_key "ratings", "videos"
+  add_foreign_key "videos", "users"
 end
