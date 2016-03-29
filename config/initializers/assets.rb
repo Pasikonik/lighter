37c1dd6cc7baf519a39fa39965f9e9c8f365a50f
Pasikonik <blind.x6@gmail.com>
2016-03-29 20:37:40 +0200

    assets magick

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(*.css *.js *.coffee *.scss *.jpg)
+Rails.application.config.assets.precompile += %w(*.png *.coffee *.scss *.jpg active_admin.css active_admin.js)
