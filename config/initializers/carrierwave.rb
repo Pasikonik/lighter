28c35fe6563037f4d0e3aeb46259c600c18606e0
Pasikonik <blind.x6@gmail.com>
2016-03-29 20:37:51 +0200

    update secrets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,9 @@
 CarrierWave.configure do |config|
-  if Rails.env.test?
-    config.storage :file
-  else
-    config.storage :fog
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['LIGHT_S3_ID'],
-      aws_secret_access_key: ENV['LIGHT_S3_KEY'],
-      region: 'eu-central-1'
-    }
-    if Rails.env.development?
-      config.fog_directory = ENV['LIGHT_S3_BUCKETS_DEV']
-    elsif Rails.env.production?
-      config.fog_directory = ENV['LIGHT_S3_BUCKETS']
-    end
-  end
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_key,
+    region: 'eu-central-1'
+  }
+  config.fog_directory = Rails.application.secrets.aws_bucket_name
 end
